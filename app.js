80e3c238b635e770ea4919a0dc0d6506957badac
santiagoGuastavino <san.guasta@gmail.com>
2021-04-06 21:08:43 -0300

    cambio servidor

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.listen(3000, () => console.log('Server running'));
+app.listen(3030, () => console.log('Server running'));
 app.use(express.static('public'));
 
 let indexPath = path.resolve('./views/index.html');
